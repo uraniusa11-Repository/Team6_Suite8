0c864188f839e8778f8c8951be5e8fa7d901dc4e
AnuVedh <anu.hari80@gmail.com>
2026-06-08 16:40:23 -0400

    Adding Contact and calender page

---
+++ Pages/ContactsPage.js
@@ -0,0 +1,328 @@
+import { expect } from '@playwright/test';
+export class ContactsPage {
+
+  constructor(page) {
+
+    this.page = page;
+
+    this.contactsMenu =page.locator('a.top-nav-link').filter({ hasText: 'Contacts' });
+    this.contactDropdownOptions =page.locator('a.sub-nav-link:visible');
+    this.createContactButton = page.getByRole('link', { name: 'Create Contact', exact: true });
+     this.createPageHeading = page.locator('scrm-dynamic-label span').first();
+   this.createContactVc = page.getByRole('link', { name: 'Create Contact From vC...' });
+   this.createVcPageHeading = page.locator('iframe').contentFrame().getByRole('heading', { name: 'Import vCard' });
+   this.viewContacts = page.getByRole('link', { name: 'View Contacts' });
+   this.viewContactPageHeading = page.getByText('CONTACTS', { exact: true });
+   this.importContact = page.getByRole('link', { name: 'Import Contacts' });
+   this.importContactaPageHeading = page.locator('iframe').contentFrame().getByRole('heading', { name: 'Step 1: Upload Import File' });
+   this.saveButton =page.getByRole('button', { name: 'Save' });
+   this.saveClickPageNavigation = page.locator('scrm-dynamic-label span.dynamic-label');
+   this.cancelButton = page.getByRole('button', { name: 'Cancel' });
+   this.popupMessage = page.locator('div.modal-body span.dynamic-label');
+   this.okButton = page.getByRole('button', { name: 'Ok' });
+   this.cancelClickPageNavigation = page.getByText('CONTACTS', { exact: true });
+   
+
+    // Form Locators
+    this.salutationDropdown = page.locator('select').first();
+    this.firstNameTextbox = page.locator('.dynamic-field-name-first_name input');
+    this.lastNameTextbox = page.locator('.dynamic-field-name-last_name input');
+    this.officePhoneTextbox = page.locator('.dynamic-field-name-phone_work input');
+   this.mobileTextbox = page.locator('.dynamic-field-name-phone_mobile input');
+   this.jobTitleTextbox = page.locator('.dynamic-field-name-title input');
+    this.departmentTextbox =  page.locator('.dynamic-field-name-department input');
+      this.accountNameTextbox = page.locator('.related-item-label');
+    this.accountDropdownArrow = page.locator('.dynamic-field-name-account_name .p-dropdown-trigger');
+    this.accountSearchTextbox = page.locator('input.p-dropdown-filter');
+    this.accountOptions = page.locator('.p-dropdown-item');
+    this.emailTextbox = page.getByRole('textbox').nth(7);
+    this.primaryAddressTextbox = page.locator('textarea').first();
+    this.lastNameValidationMessage = page.locator('.invalid-feedback span.dynamic-label');
+    this.validationBanner = page.getByText('There are validation errors');
+
+    // Save Button
+   this.saveButton = page.getByRole('button', {name: 'Save'});
+
+      //option 2 : createcontactfromVCF
+      this.createContactFromVCFOption = page.getByRole('link', {name: 'Create Contact From vC...'});
+      this.frame = page.locator('iframe').contentFrame();
+      this.chooseFileButton = this.frame.getByRole('button',{ name: 'Choose File' });
+      this.importVcardButton = this.frame.getByRole('button',{ name: 'Import vCard' });
+       this.importedContactName = page.locator('scrm-dynamic-label');
+   
+
+//Import Contacts
+
+      this.importContactsOption = page.getByRole('link', { name: 'Import Contacts' });
+      this.selectFileButton = this.frame.getByRole('button', {name: 'Select file:'});
+      this.nextButton = this.frame.getByRole('button', {name: 'Next >'});
+      this.firstNameDropdown = this.frame.locator('select[name="colnum_0"]');
+      this.lastNameDropdown = this.frame.locator('select[name="colnum_1"]');
+      this.importSettingsTextbox = this.frame.getByRole('textbox', {name: 'To save the import settings,'});
+      this.importNowButton = this.frame.getByRole('button', {name: 'Import Now'});
+      this.importResultsHeading = this.frame.getByRole('heading', {name: 'Step 5: View Import Results'});
+ // View Contacts
+      this.viewContactsLink = page.getByRole('link', {name: 'View Contacts'});
+ // Contacts heading
+      this.contactsHeading = page.getByText('CONTACTS', {exact: true});
+
+  }
+  //import contacts submodule methods
+  //import
+    async clickImportContacts() {
+
+       await this.hoverContactsMenu();
+       await this.importContactsOption.click();
+    }
+    async uploadImportFile() {
+
+       await this.selectFileButton.setInputFiles('testData/contact.vcf');
+       await this.nextButton.click();
+    }
+    async confirmFileProperties() {
+
+        await this.page.waitForLoadState('networkidle');
+        await this.nextButton.click();
+    }
+    async mapImportFields() {
+
+    await expect(this.firstNameDropdown).toBeVisible({ timeout: 30000 });
+    await this.firstNameDropdown.selectOption('first_name');
+    await this.lastNameDropdown.selectOption('last_name');
+    await this.nextButton.click();
+  }
+  async importContacts() {
+
+    await this.importSettingsTextbox.fill('anusha');
+    await this.importNowButton.click();
+  }
+  async verifyImportResults() {
+
+   await expect(
+    this.importResultsHeading
+).toBeVisible({ timeout: 30000 });
+}
+
+
+async clickCreateContactFromVcard() {
+    
+
+    await this.hoverContactsMenu();
+
+     await this.createContactFromVCFOption.waitFor({state: 'visible'});
+
+    await this.createContactFromVCFOption.click();
+
+    await this.page.waitForURL(/importvcard/,{ timeout: 30000 });
+
+    console.log(await this.page.url());
+}
+
+async verifyImportVcardPage() {
+
+    await this.page.waitForLoadState('networkidle');
+      await expect(this.createVcPageHeading).toContainText('Import vCard');
+
+        const text = await this.createVcPageHeading.textContent();
+
+        console.log("Page Heading:", text);
+    
+   
+}
+
+async uploadVcardFileMethod() {
+ 
+
+    await this.chooseFileButton.setInputFiles('data/contact.vcf');
+     await this.importVcardButton.click();
+
+  
+}
+async verifyImportedContact() {
+  await this.page.waitForLoadState('networkidle');
+  
+    await expect(this.importedContactName).toBeVisible({timeout: 120000});
+   const importedName = (await this.importedContactName.textContent())?.trim();
+
+    console.log('Imported Contact:',importedName);
+
+    expect(importedName.length).toBeGreaterThan(0);
+}
+  // Navigation Methods
+  async clickContactsMenu() {
+    
+    await this.contactsMenu.click();
+
+  }
+  async clickContactFromVCF(){
+    await this.createContactFromVCF.click();
+  }
+
+  
+   async getDropdownOptionsText() {
+
+    const options = await this.contactDropdownOptions.allTextContents();
+        const cleanedOptions =
+        options.map(option =>
+            option.replace(/\s+/g, ' ').trim()
+        );
+
+    console.log('Contacts Dropdown Options:');
+
+    cleanedOptions.forEach(option => {
+    
+    });
+     return cleanedOptions;
+  }
+
+  async clickCreateContact() {
+
+    await this.createContactButton.click();
+
+  }
+
+  async verifyCreateContactPage() {
+    await this.page.waitForLoadState('networkidle');
+      await expect(this.createPageHeading).toContainText('Create');
+      const text = await this.createPageHeading.textContent();
+        console.log("Page Heading:", text);
+    }
+
+ async hoverContactsMenu() {
+
+        await this.contactsMenu.hover();
+    }
+
+
+   async clickSaveButton() {
+   
+ 
+      const errors = await this.page.locator('.invalid-feedback').allTextContents();
+      console.log(errors);
+      await this.saveButton.scrollIntoViewIfNeeded();
+       await this.saveButton.click();
+      await this.page.waitForURL(/contacts\/record/,{ timeout: 30000 });
+   
+    }
+
+    async verifySaveNavigation(firstName,lastName) {
+
+      const savedContact = this.page.locator('.record-view-name span.dynamic-label').last();
+          await expect(savedContact).toBeVisible({ timeout: 20000 });
+      const text = await savedContact.textContent();
+        console.log("Saved Contact:", text);
+        expect(text).toContain(firstName);
+        expect(text).toContain(lastName);
+
+     }
+  async clickCancelButton() {
+
+        await this.cancelButton.click();
+    }
+async verifyPopupDisplayed() {
+
+    await expect(this.popupMessage).toBeVisible();
+    const text = await this.popupMessage.textContent();
+    console.log("Popup Message:",text);
+    expect(text).toContain('You are about to leave this record');
+}
+
+
+    async clickOkButton() {
+
+        await this.okButton.click();
+    }
+
+
+
+    async verifyCancelNavigation() {
+
+        await expect(this.cancelClickPageNavigation).toBeVisible();
+         const text = await this.cancelClickPageNavigation.textContent();
+        console.log("Navigation Text:", text);
+    }
+
+
+  
+
+ // Fill Contact Details
+ async fillContactDetails(data) {
+
+    
+    // Store Expected Name
+
+    this.expectedContactName =`${data.firstName} ${data.lastName}`;
+
+    // Fill Form
+    
+
+    await this.salutationDropdown
+      .selectOption(data.salutation);
+
+    await this.firstNameTextbox
+      .fill(data.firstName);
+
+    await this.lastNameTextbox
+      .fill(data.lastName);
+
+    await this.officePhoneTextbox
+      .fill(data.officePhone);
+
+      await this.mobileTextbox
+        .fill(data.mobile);
+
+     await this.jobTitleTextbox
+       .fill(data.jobTitle);
+
+     await this.departmentTextbox
+       .fill(data.department);
+//account
+
+await this.accountDropdownArrow.click();
+console.log("Dropdown panel count:",
+    await this.page.locator('.p-dropdown-panel').count()
+);
+await expect(this.page.locator('input.p-dropdown-filter')).toBeVisible({ timeout: 10000 });
+
+await this.page.locator('input.p-dropdown-filter').fill(data.accountName);
+
+await expect(this.page.locator('li[role="option"]').first()).toBeVisible({ timeout: 60000 });
+
+await this.page.locator('li[role="option"]').first().click();
+
+
+  }
+
+  async enterContactWithoutLastName(data) {
+
+     await this.firstNameTextbox.fill(data.firstName);
+    await this.lastNameTextbox.fill(data.lastName);
+    await this.officePhoneTextbox.fill(data.officePhone);
+         await this.saveButton.click();
+   
+}
+
+async verifyLastNameValidationMessage() {
+
+    await expect(this.lastNameValidationMessage).toBeVisible();
+    const message = await this.lastNameValidationMessage.textContent();
+    console.log('Validation Message:', message);
+    expect(message.trim()).toBe('Missing required field: Last Name');
+}
+
+async navigateToViewContacts() {
+
+    await this.contactsMenu.hover();
+    await this.viewContactsLink.click();
+}
+
+async verifyContactsPage() {
+
+    await expect(this.contactsHeading).toBeVisible();
+    const text = await this.contactsHeading.textContent();
+    console.log('Contacts Heading:',text);
+}
+  
+
+
+}
---
+++ Pages/calenderPage.js
@@ -0,0 +1,230 @@
+import { expect } from '@playwright/test';
+export class CalendarPage {
+
+    constructor(page) {
+
+    this.page = page;
+
+    // Calendar Menu
+this.calendarMenu = page.locator('a.top-nav-link.nav-link-nongrouped',{ hasText: 'Calendar' }
+);
+//Schedule Meeting
+this.scheduleMeetingLink = page.getByRole('link', {name: 'Schedule Meeting'});
+this.frame = page.locator('iframe').contentFrame();
+this.meetingsHeading = this.frame.getByText('Meetings');
+//schedule meeting form fill locators
+ this.subjectTextbox = this.frame.locator('#name');
+ this.startDateIcon = this.frame.locator('#date_start_trigger');
+ this.startHourDropdown = this.frame.locator('#date_start_hours');
+ this.startMinuteDropdown = this.frame.locator('#date_start_minutes');
+ this.endDateIcon = this.frame.locator('#date_end_trigger');
+ this.endHourDropdown = this.frame.locator('#date_end_hours');
+ this.endMinuteDropdown = this.frame.locator('#date_end_minutes');
+ this.statusDropdown = this.frame.locator('#status');
+ this.saveButton = this.frame.getByRole('button', {name: 'Save',exact: true});
+ this.createdMeetingHeading = this.frame.getByRole('heading', { name: 'accounts' })
+
+// Created Meeting Name
+  this.createdMeetingTitle = page.locator('h2.module-title-text');
+
+//Schedule call
+  this.scheduleCallLink = page.getByRole('link',{name:'Schedule Call'});
+  this.createTaskLink = page.getByRole('link',{name:'Create Task'});
+  this.todayLink = page.getByRole('link',{name:'Today'});
+
+  // Call Page
+  this.callHeading = this.frame.locator('.header-module-title');
+  this.callsubjectTextbox = this.frame.locator('#name');
+  this.callstartHourDropdown = this.frame.locator('#date_start_hours');
+  this.callstartMinuteDropdown = this.frame.locator('#date_start_minutes');
+  this.calldurationHoursTextbox = this.frame.locator('#duration_hours');
+  this.calldirectionDropdown = this.frame.locator('#direction');
+  this.callstatusDropdown = this.frame.locator('#status');
+  this.callsaveButton = this.frame.getByRole('button',{name:'Save',exact:true});
+//  this.createdCallHeading = this.frame.getByRole('heading',{name:'Accounts'});
+  
+// Task
+  this.createTaskLink = page.getByRole('link', {name: 'Create Task'});
+
+  // Task Page Heading
+    this.taskHeading = page.getByText('Create', { exact: true });
+    this.taskSubjectTextbox =  this.page.getByRole('tabpanel', { name: 'TASK OVERVIEW' }).locator('input[type="text"]');
+   
+  // Start Date Calendar
+    this.taskStartDateButton = page.getByRole('button').nth(3);
+
+// Due Date Calendar
+    this.taskDueDateButton = page.locator(
+        '.dynamic-field.dynamic-field-mode-edit.dynamic-field-name-date_due > .d-flex > .flex-grow-1 > .input-group > .input-group-append > scrm-button > .btn'
+    );
+
+// Priority
+this.priorityDropdown = page.locator('scrm-dropdownenum-edit').filter({ hasText: 'High Medium Low' })
+        .locator('select');
+
+// Status
+this.taskStatusDropdown = page.locator('scrm-dropdownenum-edit').filter({ hasText: 'Not Started In Progress' })
+        .getByRole('combobox');
+
+// Created Task
+  this.createdTaskHeading = page.locator('.dynamic-label').first();
+  this.priorityDropdown = this.page.locator('scrm-dropdownenum-edit').filter({hasText:'High Medium Low'}).locator('select');
+  this.taskStatusDropdown = this.page.locator('scrm-dropdownenum-edit').filter({hasText:'Not Started In Progress'}).locator('select');
+  this.taskSaveButton = this.page.getByRole('button',{name:'Save'});
+  this.createdTaskHeading = this.page.locator('.record-view-name-label');
+  
+  // Calendar
+  this.calendarHeading = this.frame.getByRole('heading',{name:'Calendar'});
+}
+ 
+
+//Methods
+//Schedule meeting methods
+   async navigateToScheduleMeeting() {
+
+    await this.calendarMenu.hover();
+    console.log(await this.calendarMenu.count());
+    await this.scheduleMeetingLink.waitFor({state: 'visible'});
+    await this.scheduleMeetingLink.click();
+    console.log("URL After Click:", this.page.url());
+    await this.page.waitForTimeout(3000);
+   
+}
+
+async verifyMeetingPage() {
+      await this.page.waitForLoadState('networkidle');
+    await expect(this.meetingsHeading).toContainText('Meetings');
+    const text = await this.meetingsHeading.textContent();
+    console.log("Meeting Heading:", text);
+}
+
+getStartDate(day) {
+    return this.frame.getByRole('link', {name: String(day),exact: true});
+}
+getEndDate(day) {
+    return this.frame.getByRole('cell', {name: String(day),exact: true});
+}
+
+async createMeeting(data) {
+
+    await this.subjectTextbox.fill(data.subject);
+    await this.startDateIcon.click();
+    await this.getStartDate(data.startDate).click();
+    await this.startHourDropdown.selectOption(data.startHour);
+    await this.startMinuteDropdown.selectOption(data.startMinute);
+    await this.endDateIcon.click();
+    await this.getEndDate(data.endDate).click();
+    await this.endHourDropdown.selectOption(data.endHour);
+    await this.endMinuteDropdown.selectOption(data.endMinute);
+    await this.statusDropdown.selectOption(data.status);
+
+}
+async clickSaveButton() {
+
+    await this.saveButton.click();
+}
+
+async verifyCreatedMeeting(expectedSubject) {
+
+    const meetingHeading = this.frame.getByRole('heading', {name: expectedSubject });
+    await expect(meetingHeading).toBeVisible();
+    const text = await meetingHeading.textContent();
+    console.log('Created Meeting:',text);
+}
+
+   //Navigation Methods
+    async navigateToScheduleCall() {
+
+        await this.calendarMenu.hover();
+        await this.scheduleCallLink.click();
+    }
+
+    async navigateToCreateTask() {
+
+        await this.calendarMenu.hover();
+        await this.createTaskLink.waitFor({state: 'visible'});
+        await this.createTaskLink.click();
+    }
+
+    async navigateToToday() {
+
+        await this.calendarMenu.hover();
+        await this.todayLink.click();
+    }
+
+   
+    // Verification Methods
+
+
+    async verifyCallPage() {
+
+        await expect(this.callHeading).toContainText('Calls');
+    }
+
+    async verifyTaskPage() {
+
+        await expect(this.taskHeading).toContainText('Create');
+    }
+
+    async verifyCalendarPage() {
+
+        
+        await expect(this.calendarHeading).toContainText('Calendar');
+    }
+
+   
+    // Create Call
+  
+
+    async createCall(data) {
+
+        await this.callsubjectTextbox.fill(data.subject);
+        await this.callstartHourDropdown.selectOption(data.startHour);
+        await this.callstartMinuteDropdown.selectOption(data.startMinute);
+        await this.calldurationHoursTextbox.fill(data.durationHours);
+        await this.calldirectionDropdown.selectOption(data.direction);
+        await this.callstatusDropdown.selectOption(data.status);
+        await this.callsaveButton.click();
+    }
+
+    async verifyCreatedCall(subject) {
+
+          const callHeadingNavText = this.frame.getByRole('heading').first();
+          console.log('Created Call:',await callHeadingNavText.textContent());
+          console.log("Print subject",subject); 
+          await expect(callHeadingNavText).toContainText(subject);
+    
+    }
+
+  
+    // Create Task
+   
+getTaskStartDate(day) {
+
+    return this.page.getByText(String(day),{ exact: true }).first();
+}
+
+getTaskDueDate(day) {
+
+    return this.page.getByText(String(day),{ exact: true }).first();
+}
+    async createTask(data) {
+
+        await this.taskSubjectTextbox.fill(data.subject);
+        await this.taskStartDateButton.click();
+        await this.getTaskStartDate(data.startDate).click();
+        await this.taskDueDateButton.click();
+        await this.getTaskDueDate(data.dueDate).click();
+        await this.priorityDropdown.selectOption(data.priority);
+        await this.taskStatusDropdown.selectOption(data.status);
+        await this.taskSaveButton.click();
+    }
+
+    async verifyCreatedTask(subject) {
+
+        await expect(this.createdTaskHeading).toContainText(subject);
+        const text = await this.createdTaskHeading.textContent();
+        console.log('Created Task:', text);
+    }
+}
+
